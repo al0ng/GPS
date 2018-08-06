0dd15f4558024ea0acc98c3e26d36b0db25b14c9
ningbaoqi <baoqi.ning@new-bund.net>
2018-08-06 15:30:22 +0800

    获取经纬度坐标

--- app/src/main/java/com/shop/ningbaoqi/gps/MainActivity.java
+++ app/src/main/java/com/shop/ningbaoqi/gps/MainActivity.java
@@ -1,13 +1,72 @@
 package com.shop.ningbaoqi.gps;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * 获取当前位置信息
+     *
+     * @param savedInstanceState
+     */
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        String provider = locationManager.getBestProvider(criteria, true);
+        @SuppressLint("MissingPermission") Location location = locationManager.getLastKnownLocation(provider);
+        updateWithNewLocation(location);
+        locationManager.requestLocationUpdates(provider, 2000, 10, locationListener);//每隔2000ms更新一次
     }
+
+    private void updateWithNewLocation(Location location) {
+        String latLongString;
+        TextView myLocationText = findViewById(R.id.text);
+        if (location != null) {
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+            latLongString = "维度是：" + lat + "\n经度是：" + lng;
+        } else {
+            latLongString = "失败";
+        }
+        myLocationText.setText("获取的当前位置是：\n" + latLongString);
+    }
+
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            updateWithNewLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            updateWithNewLocation(null);
+        }
+    };
 }
